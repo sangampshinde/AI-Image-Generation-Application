5c77be1645dd26615ec880bc73f223e9e54c9236
sangam shinde <sangampshinde@gmail.com>
2024-10-04 21:42:38 +0530

    design Home page and random prompt logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter,Link,Routes,Route } from 'react-router-dom';
 
-import{ logo } from './assets';
+import { logo } from './assets';
 import { Home, CreatePost } from './pages';
 
 
@@ -10,9 +10,16 @@ const App = () => {
    <>
 
     <BrowserRouter>
-    <header className='w-full flex  justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-
+    <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <Link to="/"><img  src={ logo } alt="logo" className='w-28 object-contain'/></Link>
+      <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
     </header>
+    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create-post" element={<CreatePost />} />
+    </Routes>
+    </main>
 
     
     
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt }  from "../utils";
+import { Loader, FormField } from "../components";
 
 const CreatePost = () => {
   return (
     <>
-    
     
     
     
